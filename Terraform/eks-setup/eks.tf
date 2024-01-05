7f6dbd08e36bc5f4182297b24520a6825313c04a
pyaephyohein <pyaephyohein.info.3326@gmail.com>
2024-01-05 10:39:14 +0630

    efs testing

--- Terraform/eks-setup/eks.tf
+++ Terraform/eks-setup/eks.tf
@@ -167,7 +167,7 @@ resource "aws_security_group" "allow_nfs" {
     from_port   = 2049
     to_port     = 2049
     protocol    = "tcp"
-    cidr_blocks = [cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, count.index)]
+    cidr_blocks = flatten([cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, 2), cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, 3)])
   }
 
   egress {
@@ -185,16 +185,38 @@ resource "aws_efs_file_system" "stw_node_efs" {
   creation_token = "efs-${var.name}-${var.environment}-cluster-token"
 }
 
-resource "aws_efs_mount_target" "stw_node_efs_mt_0" {
+resource "aws_efs_mount_target" "stw_node_efs_mt" {
+  count = var.availability_zones_count
   file_system_id  = aws_efs_file_system.stw_node_efs.id
-  subnet_id       = aws_subnet.private[*].id
+  subnet_id       = aws_subnet.private[count.index].id
   security_groups = [aws_security_group.allow_nfs.id]
 }
 
-resource "aws_efs_mount_target" "stw_node_efs_mt_1" {
-  file_system_id  = aws_efs_file_system.stw_node_efs.id
-  subnet_id       = aws_subnet.private[*].id
-  security_groups = [aws_security_group.allow_nfs.id]
+resource "helm_release" "aws_efs_csi_driver" {
+  chart      = "aws-efs-csi-driver"
+  name       = "aws-efs-csi-driver"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
+
+  set {
+    name  = "image.repository"
+    value = "602401143452.dkr.ecr.${var.region}.amazonaws.com/eks/aws-efs-csi-driver"
+  }
+
+  set {
+    name  = "controller.serviceAccount.create"
+    value = true
+  }
+
+  set {
+    name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.attach_efs_csi_role.iam_role_arn
+  }
+
+  set {
+    name  = "controller.serviceAccount.name"
+    value = "efs-csi-controller-sa"
+  }
 }
 
 
